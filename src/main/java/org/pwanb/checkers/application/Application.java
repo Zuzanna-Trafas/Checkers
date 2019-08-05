258947ab108f2ae1e2af0612c3d94b9c9c0524ca
Wiktor Górczak <wiktor.gorczak@openanalytics.eu>
2019-08-05 19:47:45 +0200

    Initial commit.

---
+++ src/main/java/org/pwanb/checkers/application/Application.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package org.pwanb.checkers.application;
+
+import de.gurkenlabs.litiengine.Game;
+
+/**
+ * @author Codename "P.W.A.N.B"
+ *
+ */
+public class Application {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Game.init();
+		Game.start();
+	}
+
+}

3a89b9ed222876653390cf1df9b2f787d753a662
MichalSz99 <m.szczepaniak99@gmail.com>
2019-08-11 12:02:06 +0200

    New Android Studio Project

---
+++ app/src/main/java/org/pwanb/checkers/application/MainActivity.java
@@ -0,0 +1,20 @@
+/**
+ * ALL DAMN RIGHTS RESERVED. !!!! 2019 (R)
+ */
+package org.pwanb.checkers.application;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * @author Codename "P.W.A.N.B"
+ */
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}

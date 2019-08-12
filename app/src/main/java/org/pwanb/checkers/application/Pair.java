d2757eb41cf8cb9f178f3d8823f628cadcb97d61
MichalSz99 <m.szczepaniak99@gmail.com>
2019-08-12 12:34:04 +0200

    New ImageView for every field of Board

---
+++ app/src/main/java/org/pwanb/checkers/application/Pair.java
@@ -0,0 +1,36 @@
+package org.pwanb.checkers.application;
+
+public final class Pair {
+    private boolean set;
+    private int X;
+    private int Y;
+
+    public Pair()
+    {
+        set = false;
+    }
+
+    public Pair(int x, int y)
+    {
+        set = true;
+        X = x;
+        Y = y;
+    }
+
+    public int getX() {
+        return X;
+    }
+
+    public int getY() {
+        return Y;
+    }
+
+    public void set(int x, int y) {
+        X = x;
+        Y = y;
+    }
+
+    boolean isSet(){
+        return set;
+    }
+}

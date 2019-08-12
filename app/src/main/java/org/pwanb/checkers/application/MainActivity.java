e4c37ac9af48070f78d7af4bbe5de918ca1709d1
MichalSz99 <m.szczepaniak99@gmail.com>
2019-08-12 10:05:18 +0200

    Add Drawable highlight

--- app/src/main/java/org/pwanb/checkers/application/MainActivity.java
+++ app/src/main/java/org/pwanb/checkers/application/MainActivity.java
@@ -3,15 +3,21 @@
  */
 package org.pwanb.checkers.application;
 
+import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.view.View;
+
+
 
 /**
  * @author Codename "P.W.A.N.B"
  */
 
 public class MainActivity extends AppCompatActivity {
-
+    ImageView imv;
+    boolean click = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);

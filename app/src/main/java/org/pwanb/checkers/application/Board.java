12280dfb03ac7e914eb6cc37eb6d39c650287706
MichalSz99 <m.szczepaniak99@gmail.com>
2019-08-13 13:19:46 +0200

    Highlight possible option of move added

--- app/src/main/java/org/pwanb/checkers/application/Board.java
+++ app/src/main/java/org/pwanb/checkers/application/Board.java
@@ -7,46 +7,23 @@ import android.graphics.drawable.Drawable;
 import android.support.v7.widget.AppCompatImageView;
 
 class Board{
+    private boolean whiteTurn;
     private Pair chosenField;
     private Field[][] board = new Field[8][8];
     private Activity activity;
+    private Pawn[] white = new Pawn[12];
+    private Pawn[] black = new Pawn[12];
 
     class Field implements AppCompatImageView.OnClickListener {
 
         private ImageView image;
-        private int X;
-        private int Y;
+        private Pair position;
+        private Pawn pawn = null;
 
         Field(int x, int y, ImageView img) {
-            X = x;
-            Y = y;
+            position = new Pair(x,y);
             image = img;
             image.setOnClickListener(this);
-        }
-
-        @Override
-        public void onClick(View v) {
-            int prevX = chosenField.getX();
-            int prevY = chosenField.getY();
-            System.out.println(X + "" + Y);
-            if (chosenField.isSet()) {
-                if (X != prevX || Y != prevY) {
-
-                    board[prevX][prevY].deleteHighlight();
-                    this.setHighlight();
-                    chosenField.set(X, Y);
-                } else {
-                    this.deleteHighlight();
-                    chosenField.unset();
-                }
-            } else {
-                this.setHighlight();
-                chosenField.set(X, Y);
-            }
-        }
-
-        void deleteHighlight() {
-            image.setBackground(null);
         }
 
         void setHighlight() {
@@ -54,10 +31,91 @@ class Board{
             image.setBackground(highlight);
         }
 
-        void setImage(final int imageID) {
+        void deleteHighlight() {
+            image.setBackground(null);
+        }
+
+        void setPawn(final int x, final int y, final boolean white,final int imageID ) {
+            this.pawn = new Pawn(x, y, white);
             image.setImageResource(imageID);
         }
+
+        void deletePawn(){
+            pawn = null;
+            image.setImageResource(PawnGraphics.EMPTY.get());
+        }
+
+
+        @Override
+        public void onClick(View v) {
+            if (pawn != null)
+                firstClick();
+            else if (chosenField.isSet())
+                secondClick();
+
+        }
+
+        private void firstClick() {
+            int prevX = chosenField.getX();
+            int prevY = chosenField.getY();
+            int X = position.getX();
+            int Y = position.getY();
+            if (pawn.isWhite() == whiteTurn) {
+                if (chosenField.isSet()) {
+                    if (X != prevX || Y != prevY) {
+                        board[prevX][prevY].deleteHighlight();
+                        board[prevX][prevY].deleteOption();
+                        setHighlight();
+                        chosenField.set(X, Y);
+                        showOption();
+                    } else {
+                        deleteHighlight();
+                        deleteOption();
+                        chosenField.unset();
+                    }
+                } else {
+                    setHighlight();
+                    chosenField.set(X, Y);
+                    showOption();
+                }
+            }
+        }
+
+        private void secondClick() {
+            int prevX = chosenField.getX();
+            int prevY = chosenField.getY();
+            int X = position.getX();
+            int Y = position.getY();
+
+
+        }
+
+        void showOption(){
+            int x;
+            int y;
+            for(int i = 0 ; i< pawn.getMoveOption(); i++)
+            {
+                x = pawn.getPossibleMove()[i].getX();
+                y = pawn.getPossibleMove()[i].getY();
+                board[x][y].setHighlight();
+            }
+        }
+
+        void deleteOption(){
+            int x;
+            int y;
+            for(int i = 0 ; i< pawn.getMoveOption(); i++)
+            {
+                x = pawn.getPossibleMove()[i].getX();
+                y = pawn.getPossibleMove()[i].getY();
+                board[x][y].deleteHighlight();
+            }
+        }
+
+
     }
+
+
 
     Board(ImageView[][] boardMain, Activity activity) {
         chosenField = new Pair();
@@ -71,13 +129,98 @@ class Board{
     }
 
     private void start() {
-        for (int i = 0; i < 8; i += 2) {
-            board[i][0].setImage(PawnGraphics.WHITE_PAWN.get());
-            board[i + 1][1].setImage(PawnGraphics.WHITE_PAWN.get());
-            board[i][2].setImage(PawnGraphics.WHITE_PAWN.get());
-            board[i + 1][5].setImage(PawnGraphics.BLACK_PAWN.get());
-            board[i][6].setImage(PawnGraphics.BLACK_PAWN.get());
-            board[i + 1][7].setImage(PawnGraphics.BLACK_PAWN.get());
+        whiteTurn = true;
+        for (int i = 0; i < 4; i++) {
+            board[2*i][0].setPawn(2*i, 0, true, PawnGraphics.WHITE_PAWN.get());
+            board[2*i + 1][1].setPawn(2*i+1, 1, true, PawnGraphics.WHITE_PAWN.get());
+            board[2*i][2].setPawn(2*i, 2, true,PawnGraphics.WHITE_PAWN.get());
+            board[2*i + 1][5].setPawn(2*i+1, 5, false,PawnGraphics.BLACK_PAWN.get());
+            board[2*i][6].setPawn(2*i, 6, false,PawnGraphics.BLACK_PAWN.get());
+            board[2*i + 1][7].setPawn(2*i+1, 7, false,PawnGraphics.BLACK_PAWN.get());
+            white[3*i] = new Pawn(board[2*i][0].pawn);
+            white[3*i+1]= new Pawn(board[2*i + 1][1].pawn);
+            white[3*i+2]= new Pawn(board[2*i][2].pawn);
+            black[3*i] =  new Pawn(board[2*i+1][5].pawn);
+            black[3*i+1]= new Pawn(board[2*i][6].pawn);
+            black[3*i+2]= new Pawn(board[2*i+1][7].pawn);
+        }
+        this.possibleMoves();
+    }
+
+    private void possibleMoves(){
+        if(whiteTurn)
+        {
+            update(white);
+        }
+        else{
+            update(black);
+        }
+    }
+
+    private void update(Pawn[] pawns){
+        boolean empty = true;
+
+        for (int i =0; i<12; i++)
+        {
+            if(pawns[i] != null){
+                empty = false;
+                int x = pawns[i].getCurrentPosition().getX();
+                int y = pawns[i].getCurrentPosition().getY();
+                int itr = 0;
+                System.out.println(x+ " "+ y );
+                if (pawns[i].isKing())
+                {
+                    //TODO king move
+                }
+                else
+                {
+                    if(y < 7)
+                    {
+                        if(x<7) {
+                            if(board[x+1][y+1].pawn == null)
+                            {
+
+                                pawns[i].setPossibleMove(itr, new Pair(x+1,y+1));
+                                board[x][y].pawn.setPossibleMove(itr, new Pair(x+1,y+1));
+                                itr++;
+                            }
+                            else{
+                                //TODO attack option
+                                // wyslc do innej listy o wyzszym priorytecie
+                            }
+                        }
+                        if(x > 0){
+                            if(board[x-1][y+1].pawn == null)
+                            {
+                                pawns[i].setPossibleMove(itr, new Pair(x-1,y+1));
+                                board[x][y].pawn.setPossibleMove(itr, new Pair(x-1,y+1));
+                            }
+                            else{
+                                //TODO attack option
+                                // wyslc do innej listy o wyzszym priorytecie
+                            }
+                        }
+                    }
+
+                    if(y > 0)
+                    {
+                        if(x<7 && board[x+1][y-1].pawn == null && board[x+1][y-1].pawn.isWhite() != whiteTurn) {
+
+                                //TODO attack option
+                                // wyslc do innej listy o wyzszym priorytecie
+                        }
+                        if(x>0 && board[x-1][y-1].pawn == null && board[x-1][y-1].pawn.isWhite() != whiteTurn) {
+
+                            //TODO attack option
+                            // wyslc do innej listy o wyzszym priorytecie
+                        }
+
+                    }
+                }
+            }
+        }
+        if(empty) {
+            //TODO stop game
         }
     }
 }
---
+++ app/src/main/java/org/pwanb/checkers/application/Pawn.java
@@ -0,0 +1,39 @@
+package org.pwanb.checkers.application;
+
+class Pawn {
+    private boolean king;
+    private boolean white;
+    private Pair currentPosition;
+    private int moveOption;
+    private Pair[] possibleMove;
+
+    Pawn(int x, int y, boolean white){
+        king = false;
+        this.white = white;
+        currentPosition = new Pair(x,y);
+        possibleMove = new Pair[13];
+    }
+
+    Pawn(Pawn oldPawn){
+        white = oldPawn.white;
+        king = oldPawn.king;
+        moveOption = oldPawn.moveOption;
+        currentPosition = new Pair(oldPawn.currentPosition.getX(),oldPawn.currentPosition.getY());
+        possibleMove = new Pair[13];
+    }
+
+    void setPossibleMove(int itr, Pair possibleMove) {
+        this.possibleMove[itr] = possibleMove;
+        moveOption = itr + 1;
+    }
+
+    Pair[] getPossibleMove() { return possibleMove; }
+
+     int getMoveOption() { return moveOption; }
+
+    Pair getCurrentPosition() { return currentPosition; }
+
+    boolean isKing() { return king; }
+
+    boolean isWhite() { return white; }
+}
